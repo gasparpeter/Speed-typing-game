12c4a76bf2707addd6438bbde6e4fb73b6c513da
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-17 15:57:45 +0300

    - created function check status, countdown, match words, and start match

--- script.js
+++ script.js
@@ -43,10 +43,53 @@ const words = [
 function init() {
     console.log("init");
     showWord(words);
+
+    wordInput.addEventListener('input', startMatch);
+
+    setInterval(countdown, 1000);
+
+    setInterval(checkStatus, 50);
+}
+
+function startMatch() {
+    if (matchWords()) {
+        isPlaying = true;
+        time = 6;
+        showWord(words);
+        wordInput.value = '';
+        score++;
+    }
+    scoreDisplay.innerHTML = score;
+}
+
+function matchWords() {
+    if (wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!!!';
+        return true;
+    }else {
+        message.innerHTML = '';
+        return false;
+    }
 }
 
 function showWord(words) {
     const randIndex = Math.floor(Math.random() * words.length);
 
     currentWord.innerHTML = words[randIndex];
+}
+
+function countdown() {
+    if (time > 0) {
+        time--;
+    }else if (time === 0) {
+        isPlaying = false;
+    }
+
+    timeDisplay.innerHTML = time;
+}
+
+function checkStatus() {
+    if (!isPlaying && time === 0) {
+        message.innerHTML = 'Game Over!!!'
+    }
 }
