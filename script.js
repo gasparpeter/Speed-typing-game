1082966ca280a0188a37485953b9f9f64a9ea019
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-17 15:15:26 +0300

    -created elements in javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+let time = 5;
+let score = 0;
+let isPlaying;
+
+
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time-display");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
+
+const words = [
+    'hat',
+    'river',
+    'lucky',
+    'statue',
+    'generate',
+    'stubborn',
+    'cocktail',
+    'runaway',
+    'joke',
+    'developer',
+    'establishment',
+    'hero',
+    'javascript',
+    'nutrition',
+    'revolver',
+    'echo',
+    'siblings',
+    'investigate',
+    'horrendous',
+    'symptom',
+    'laughter',
+    'magic',
+    'master',
+    'space',
+    'definition'
+];
